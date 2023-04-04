920584a0ba9c76e692335012ec3a70c0cbd3143d
Stephanie <stephaniereyes@pursuit.org>
2023-04-04 13:52:16 -0400

    created nav bar items

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+import "./Navbar.css"
+
+
+const Navbar = () => {
+
+    return (
+        <div className="navbar">
+         <ol className="navItems">
+         <li> Simple Budgeting </li> 
+         <li> Transactions</li>
+         <li> Budget Goals </li>
+         <li> Reports</li>
+         <li> Sign Up 
+         Sign In 
+         </li>
+         
+         </ol>
+
+        </div>
+
+
+    )
+}
+
+export default  Navbar
